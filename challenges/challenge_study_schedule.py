f1dcd6a1c01b1088b8b1e210580b5589104419f3
Alex <alexsandrolima991@hotmail.com>
2023-04-25 17:15:58 -0300

    (feat): Implements Study Schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,4 +1,15 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    # commit inicial
-    raise NotImplementedError
+    counter = 0
+
+    if not isinstance(target_time, int) or not isinstance(
+        permanence_period, list
+    ):
+        return None
+
+    for i in permanence_period:
+        if not isinstance(i[0], int) or not isinstance(i[1], int):
+            return None
+        if i[0] <= target_time <= i[1]:
+            counter += 1
+
+    return counter
