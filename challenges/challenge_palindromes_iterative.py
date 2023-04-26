90b35c1247055f21e58cf4b40a0f69722d761e65
Alex <alexsandrolima991@hotmail.com>
2023-04-26 08:13:57 -0300

    (feat): Implements Palindromes iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,14 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(word, str) or len(word) == 0:
+        return False
+
+    low = 0
+    high = len(word) - 1
+
+    while low < high:
+        if word[low] != word[high]:
+            return False
+        low += 1
+        high -= 1
+
+    return True
